04ec9adb6d03b71068e38d36f45ba1d78216192d
Hielke Morsink <hielke.morsink@gmail.com>
2021-09-02 21:52:08 +0200

    Remove invalid test code

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-
-  core.set
 } catch (error) {
   core.setFailed(error.message);
 }
